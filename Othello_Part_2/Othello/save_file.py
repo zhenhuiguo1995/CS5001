a9c7449b84405de41ce91dd276eeb8416f8b291f
zhenhuiguo1995 <njugzh@gmail.com>
2018-11-16 22:09:29 -0800

    finished save file

--- Othello_Part_2/Othello/save_file.py
+++ Othello_Part_2/Othello/save_file.py
@@ -3,17 +3,24 @@ import sys
 
 
 def save_file():
-    file = open('scores.txt', 'w+')
-    temp = []
-    for line in file:
-        print(line)
-        #line = line.rstrip().split()
-        #temp.append((line[0], line[1]))
-    #temp.append((sys.argv[1], sys.argv[2]))
-    #print(temp)
-    #sorted(temp, key=lambda x: x[1], reverse=True)
-    #for tuple in temp:
-    #    file.write(tuple[0] + " " + tuple[1])
-
+    if 'scores.txt' not in os.listdir():
+        file = open('scores.txt', 'w+')
+        temp = sys.argv[1] + sys.argv[2] + '\n'
+        file.write(temp)
+        file.close()
+    else:
+        file = open('scores.txt')
+        temp = []
+        for line in file:
+            line = line.split()
+            temp.append((line[0], int(line[1])))
+        temp.append((sys.argv[1], int(sys.argv[2])))
+        file.close()
+        temp = sorted(temp, key=lambda x: x[1], reverse=True)
+        file = open('scores.txt', 'w+')
+        for pair in temp:
+            line = pair[0] + ' ' + str(pair[1]) + '\n'
+            file.write(line)
+        file.close()
 
 save_file()
